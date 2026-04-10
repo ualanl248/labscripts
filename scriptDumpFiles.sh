d8551c86dd8ebcd861451765643a37a6fdca0383
ualanl248 <adolfo.nogueslopezz@gmail.com>
2026-04-10 10:33:23 +0200

    Introduzco variables

--- scriptDumpFiles.sh
+++ scriptDumpFiles.sh
@@ -4,12 +4,14 @@ YELLOW='\e[33m'
 BLUE='\e[34m'
 NC='\e[0m'
 
+RUTA=''
+
 mkdir -p ~/outputs/archivos
 
 mapfile -t offsets < <(cat filescan.txt | grep "DVWA" | awk '{print $1}')
 
 for offset in "${offsets[@]}"; do
-  vol.py -f /media/adolfo/DATA/Ubuntu/temp_extract_dir/Breach/memdump.mem --profile=VistaSP1x86 dumpfiles -Q $offset -D ~/outputs/archivos
+  vol.py -f $RUTA --profile=VistaSP1x86 dumpfiles -Q $offset -D ~/outputs/archivos
 done
 
 mapfile -t archivos < <(ls -l archivos| awk 'NR>1 {print $9}' | awk '{print $1}')
